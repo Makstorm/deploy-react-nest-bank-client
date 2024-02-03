ba1d43d965a59d285c2afa80c452f7c282eece67
Makstorm <mxm.horobets@gmail.com>
2024-02-03 20:10:56 +0200

    fix: created different forms for any kind of auth pages, whitch helped to unload AuthPage that were god page
    BREAKING CHANGE: used Formik library to make forms

---
+++ src/components/Forms/LoginForm/index.tsx
@@ -0,0 +1,93 @@
+import { Form, Formik, FormikProps } from "formik";
+import FormInput from "../FormInput";
+import { loginSchema } from "../Validations/LoginValidation";
+import ChangeAuth from "../../Auth/ChangeAuth";
+import "../index.scss";
+import { useLocation } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../../hooks/redux";
+import { fetchUser } from "../../../store/reducers/ActionCreators";
+import { BallTriangle } from "react-loader-spinner";
+import AuthError from "../../Auth/AuthErrror";
+import { useEffect } from "react";
+import { toggleError } from "../../../store/reducers/UserSlice";
+
+interface Values {
+  password: string;
+  email: string;
+}
+
+const LoginForm = () => {
+  const location = useLocation();
+  const { error, isLoading } = useAppSelector((state) => state.userReduser);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(toggleError());
+  }, []);
+
+  return (
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      onSubmit={async (values, actions) => {
+        dispatch(fetchUser(values));
+        actions.resetForm();
+      }}
+      validationSchema={loginSchema}
+    >
+      {(props: FormikProps<Values>) => (
+        <Form style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+          <FormInput
+            label="Email"
+            name="email"
+            type="text"
+            placeholder="Enter your email"
+          />
+
+          <FormInput
+            label="Password"
+            name="password"
+            type="password"
+            placeholder="Enter your password"
+          />
+
+          {(location.pathname === "/signup" ||
+            location.pathname === "/signin") && (
+            <ChangeAuth
+              text={
+                location.pathname === "/signup"
+                  ? "Already have an account?"
+                  : "Forgot your password?"
+              }
+            />
+          )}
+
+          <button
+            type="submit"
+            disabled={props.isSubmitting}
+            className={`button `}
+          >
+            Continue
+          </button>
+
+          {error ? <AuthError>{error}</AuthError> : null}
+          {isLoading && (
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <BallTriangle
+                height={40}
+                width={40}
+                radius={5}
+                color="#5b94e9"
+                ariaLabel="ball-triangle-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+              />
+            </div>
+          )}
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default LoginForm;
--- src/pages/AuthPAge/index.tsx
+++ src/pages/AuthPAge/index.tsx
@@ -1,72 +1,35 @@
 import { useLocation, useNavigate } from "react-router-dom";
-import AuthInput from "../../components/Auth/AuthInput";
-import { InputType } from "../../components/Auth/AuthInput/type.enum";
 import BackArrow from "../../components/Navigations/BackwardArrow";
-import ChangeAuth from "../../components/Auth/ChangeAuth";
-import ConfirmButton from "../../components/Navigations/ConfirmButton";
 import PageInfoTitle from "../../components/Phone/PageInfoTitle";
 import PhonePage from "../../components/Phone/PhonePage";
 import PhonePageContent from "../../components/Phone/PhonePageContent";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { BALANCE_ROUTE, SIGNIN_ROUTE } from "../../components/AppRouter/consts";
-import AuthError from "../../components/Auth/AuthErrror";
 import { useAppDispatch, useAppSelector } from "../../hooks/redux";
-import {
-  fetchUser,
-  fetchUserRegister,
-} from "../../store/reducers/ActionCreators";
-import {
-  setUserError,
-  toggleError,
-  toggleIsRegistered,
-} from "../../store/reducers/UserSlice";
-import { userAPI } from "../../store/services/UserService";
-import { BallTriangle } from "react-loader-spinner";
+import { toggleIsRegistered } from "../../store/reducers/UserSlice";
+import LoginForm from "../../components/Forms/LoginForm";
+import SignUpForm from "../../components/Forms/SingUpFrom";
+import RecoveryForm from "../../components/Forms/RecoveryForm";
 
 const AuthPage = () => {
-  const [passwordRecovery, { error: error1, isSuccess, isLoading: Loading1 }] =
-    userAPI.useUserPasswordRecoverMutation();
-
   const dispatch = useAppDispatch();
 
-  const { error, isAuth, isLoading, isRegistered } = useAppSelector(
-    (state) => state.userReduser
-  );
-
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-    text: "",
-  });
-
-  const onChange = (name: string, value: string) => {
-    setFormData({ ...formData, [name]: value });
-  };
+  const { isAuth, isRegistered } = useAppSelector((state) => state.userReduser);
 
   // Utils functions
   const location = useLocation();
-  const isLogin = location.pathname === SIGNIN_ROUTE;
   const navigate = useNavigate();
-
-  useEffect(() => {
-    dispatch(toggleError());
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [formData]);
 
   useEffect(() => {
     if (isAuth) {
       navigate(BALANCE_ROUTE);
     }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isAuth]);
 
   useEffect(() => {
     if (isRegistered && location.pathname === SIGNIN_ROUTE) {
       dispatch(toggleIsRegistered());
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isRegistered]);
 
   const getTitle = (): { title: string; subtitle: string } => {
@@ -92,76 +55,6 @@ const AuthPage = () => {
     }
   };
 
-  const auth = async () => {
-    if (isLogin) {
-      if (formData.email && formData.password) {
-        dispatch(
-          fetchUser({ email: formData.email, password: formData.password })
-        );
-      } else {
-        dispatch(setUserError("Missing credentials"));
-      }
-    } else {
-      if (formData.email && formData.password && formData.text) {
-        dispatch(
-          fetchUserRegister({
-            email: formData.email,
-            password: formData.password,
-            username: formData.text,
-          })
-        );
-      } else {
-        dispatch(setUserError("Missing credentials"));
-      }
-    }
-  };
-
-  const getButton = (): {
-    outline: boolean;
-    children: React.ReactNode;
-    onClick: React.MouseEventHandler<HTMLButtonElement>;
-  } => {
-    switch (location.pathname) {
-      case "/signup": {
-        return {
-          outline: false,
-          onClick: () => {
-            auth();
-          },
-          children: "Continue",
-        };
-      }
-
-      case "/signin": {
-        return {
-          outline: false,
-          onClick: () => {
-            auth();
-          },
-          children: "Continue",
-        };
-      }
-
-      case "/recovery": {
-        return {
-          outline: false,
-          onClick: () => {
-            passwordRecovery(formData.email);
-          },
-          children: "Change password",
-        };
-      }
-
-      default: {
-        return {
-          outline: false,
-          onClick: () => {},
-          children: "Continue",
-        };
-      }
-    }
-  };
-
   return (
     <PhonePage>
       <section style={{ marginTop: "50px", padding: "0 20px" }}>
@@ -172,87 +65,21 @@ const AuthPage = () => {
       <PhonePageContent>
         {location.pathname === "/signup" && (
           <>
-            <AuthInput
-              onInputChange={onChange}
-              title="Email"
-              type={InputType.EMAIL}
-            />
-            <AuthInput
-              onInputChange={onChange}
-              title="Nickname"
-              type={InputType.TEXT}
-            />
-            <AuthInput
-              onInputChange={onChange}
-              title="Password"
-              type={InputType.PASSWORD}
-            />{" "}
+            <SignUpForm />
           </>
         )}
 
         {location.pathname === "/signin" && (
           <>
-            <AuthInput
-              onInputChange={onChange}
-              title="Email"
-              type={InputType.EMAIL}
-            />
-            <AuthInput
-              onInputChange={onChange}
-              title="Password"
-              type={InputType.PASSWORD}
-            />{" "}
+            <LoginForm />
           </>
         )}
 
         {location.pathname === "/recovery" && (
           <>
-            <AuthInput
-              onInputChange={onChange}
-              title="Email"
-              type={InputType.EMAIL}
-            />
+            <RecoveryForm />
           </>
         )}
-
-        {(location.pathname === "/signup" ||
-          location.pathname === "/signin") && (
-          <ChangeAuth
-            text={
-              location.pathname === "/signup"
-                ? "Already have an account?"
-                : "Forgot your password?"
-            }
-          />
-        )}
-
-        <ConfirmButton {...getButton()}></ConfirmButton>
-
-        {error1 ? <AuthError>Something wrong happend</AuthError> : null}
-        {error ? <AuthError>{error}</AuthError> : null}
-        {(isLoading || Loading1) && (
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <BallTriangle
-              height={40}
-              width={40}
-              radius={5}
-              color="#5b94e9"
-              ariaLabel="ball-triangle-loading"
-              wrapperStyle={{}}
-              wrapperClass=""
-              visible={true}
-            />
-          </div>
-        )}
-        {isSuccess ? (
-          <AuthError success>Email password recovery sent</AuthError>
-        ) : null}
-
-        {isRegistered ? (
-          <AuthError success>
-            Registration successfull! Sent email confirm lint to Your Email
-          </AuthError>
-        ) : null}
       </PhonePageContent>
     </PhonePage>
   );
